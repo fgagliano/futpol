a47e0f337da07be6b7dba826e41f8ac5aafcd9a6
fgagliano <fabiogagliano@terra.com.br>
2026-01-22 22:31:51 -0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>FutPol</h1>
-      <p>Deploy ok. Próximo passo: tabela do bloco atual.</p>
-    </main>
-  );
-}
